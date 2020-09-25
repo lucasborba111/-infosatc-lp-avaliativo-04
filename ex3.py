817f03ae13cc6ef97e168febc6b49d42044e6968
lucasborba111 <lucasdbb.12@gmail.com>
2020-09-25 12:03:23 -0300

    feat: exercicio3 :rocket:

---
+++ ex3.py
@@ -0,0 +1,5 @@
+def funcao(nome):
+    print(' Você digitou a palavra: {}'.format(palavra))
+    print("Palavra invertida:{}".format(palavra[::-1]))
+palavra = input("Digite uma palavra")
+funcao(palavra)
