a3f9495c263751fd1bd278d18c812e6e799ddc25
lucasborba111 <lucasdbb.12@gmail.com>
2020-09-26 00:26:59 -0300

    feat: ex4 :rocket:

---
+++ ex4.py
@@ -0,0 +1,10 @@
+def funcao(palavra1):   
+   if(palavra1[::-1]==palavra2):
+      print(palavra1,"é inversa da outra palavra",palavra2)
+   else:
+      print(palavra1,"não é inversa da outra palavra",palavra2)
+palavra1 = input("Digite uma palavra")
+palavra2 = input("Digite outra palavra")
+funcao(palavra1)
+
+   
